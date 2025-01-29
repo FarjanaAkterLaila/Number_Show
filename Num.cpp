552183dd9e6edc57d9a78d9d86ad2a63a74b0acc
FarjanaAkterLaila <farjanaakter7275@gmail.com>
2025-01-29 21:02:02 +0600

    print number

--- Num.cpp
+++ Num.cpp
@@ -1,20 +1,11 @@
+
 #include <iostream>
 using namespace std;
 
-void printFibonacci(int n) {
-    int a = 0, b = 1, c;
-    cout << a << " " << b << " ";
-    for (int i = 2; i < n; i++) {
-        c = a + b;
-        cout << c << " ";
-        a = b;
-        b = c;
-    }
-    cout << endl;
-}
-
 int main() {
-    int n = 10; 
-    printFibonacci(n);
+    cout << "Print numbers 1 to 10" << endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << i << " ";
+    }
     return 0;
 }
